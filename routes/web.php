84d5e1575a839bd3058c4f2558b26c7e68296a25
Daniel Sanchez <danielsanchezc6@gmail.com>
2021-01-19 20:35:17 +0100

    .

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('inicio');
 });
+
+Route::get('iniciarSesion', function () {
+    return view('iniciarSesion');
+});
