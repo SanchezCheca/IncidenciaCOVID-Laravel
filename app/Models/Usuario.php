86bbdb77cd2d6f2a428e011a3f102baac8197a2c
Daniel Sanchez <danielsanchezc6@gmail.com>
2021-01-20 19:53:58 +0100

    Modelo de Usuario creado

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -5,9 +5,55 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Usuario extends Model
-{
-    public $timestamps = false;
+class Usuario extends Model {
 
+    //--------------------ATRIBUTOS
+    private $id;
+    private $semana;
+    private $region;
+    private $nInfectados;
+    private $nFallecidos;
+    private $nAltas;
+    private $idAutor;
+
+    //--------------------CONSTRUCTOR
+    function __construct($timestamps, $semana, $region, $nInfectados, $nFallecidos, $nAltas, $idAutor) {
+        $this->timestamps = $timestamps;
+        $this->semana = $semana;
+        $this->region = $region;
+        $this->nInfectados = $nInfectados;
+        $this->nFallecidos = $nFallecidos;
+        $this->nAltas = $nAltas;
+        $this->idAutor = $idAutor;
+    }
     
+    //--------------------GETTER
+    function getTimestamps() {
+        return $this->timestamps;
+    }
+
+    function getSemana() {
+        return $this->semana;
+    }
+
+    function getRegion() {
+        return $this->region;
+    }
+
+    function getNInfectados() {
+        return $this->nInfectados;
+    }
+
+    function getNFallecidos() {
+        return $this->nFallecidos;
+    }
+
+    function getNAltas() {
+        return $this->nAltas;
+    }
+
+    function getIdAutor() {
+        return $this->idAutor;
+    }    
+
 }
